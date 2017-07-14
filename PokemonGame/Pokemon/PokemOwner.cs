be72de094a4241590fcfae2215bfbff58d6ebf28
unknown <sarah1109>
2017-07-14 15:13:25 +0800

    init commit

---
+++ PokemonGame/Pokemon/PokemOwner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    public class PokemonOwner
+    {
+
+        public string name; //寶可夢名
+        public string sex;//寶可夢性別
+        public int hp;//血量
+        public double weight;//重量
+        public string typeKind;//寶可夢類型
+        public double hight;//重量
+        public string typegifpath;//寶可夢類型呈現背景圖路徑
+        public int stardust;//目前星塵
+        public int candy;//目前糖果
+        public int powerupNeedStardust;//力量強化所需星塵
+        public int powerupNeedCandy;//力量強化所需糖盟
+        public int eveolevNeedCandy;//進化所需糖果
+        public string quickAttackName;//快速攻擊小技名
+        public int quickAttackLevel;//快速攻擊小技傷害值
+        public string digAttackName;//重攻擊大技傷害名
+        public int digAttackNameLevel;//重攻擊大技傷害值
+        public string gpsAddress;//目前所在位置
+        public string dateNow;//目前日期
+
+    }
+}
---
+++ PokemonGame/PokemonLibrary/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pokemon;
+
+
+namespace PokemonLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PokemonOwner atony = new PokemonOwner();
+            atony.name = "妙娃種子";
+            atony.hp = 60;
+            atony.sex = "M";
+            atony.weight = 5.94;
+            atony.typeKind = "草/毒";
+            atony.hight = 0.67;
+            atony.stardust = 271484;
+            atony.candy = 27;
+            atony.powerupNeedStardust = 3000;
+            atony.powerupNeedCandy = 3;
+            atony.eveolevNeedCandy = 25;
+            atony.quickAttackName = "藤鞭";
+            atony.quickAttackLevel = 7;
+            atony.digAttackName = "污泥炸彈";
+            atony.digAttackNameLevel = 80;
+            atony.gpsAddress = "台灣,新北市三重區";
+            atony.dateNow = DateTime.Now.ToShortDateString();
+            Console.WriteLine
+            ("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}\n{10}\n{11}",
+            atony.name,
+            atony.hp,
+            atony.sex,
+            atony.weight,
+            atony.typeKind,
+            atony.hight,
+            atony.stardust,
+            atony.candy,
+            atony.powerupNeedStardust,
+            atony.powerupNeedCandy,
+            atony.eveolevNeedCandy,
+            atony.quickAttackName,
+            atony.quickAttackLevel,
+            atony.digAttackName,
+            atony.digAttackNameLevel,
+            atony.gpsAddress,
+            atony.dateNow
+            );
+
+
+
+        }
+            
+    }
+}

7527b595be2140a8bd5e2001065cc7292633234e
tonyto5268 <tonyto5268@gmail.com>
2017-07-14 20:03:49 +0800

    Add Lecture02 Example

---
+++ Lecture02/Example01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入數字1:");
+            int num1 =int.Parse(Console.ReadLine());
+            Console.Write("請輸入數字2:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}",num1,num2,num1+num2);
+        }
+    }
+}
